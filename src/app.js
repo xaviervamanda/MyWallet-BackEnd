82a6a71ea876dc928d9deb06ec968452a4dc72fd
Amanda Xavier <amandavxavier@gmail.com>
2023-04-21 21:56:59 -0300

    Requisito: rota POST /transaction/:type

--- src/app.js
+++ src/app.js
@@ -32,6 +32,13 @@ const userSchema = joi.object({
     password: joi.string().min(3).required()
 });
 
+const transactionSchema = joi.object({
+    token: joi.string().required(),
+    value: joi.string().pattern(/^\d+(\.\d{1,2})?$/).required(),
+    description: joi.string().required(),
+    type: joi.string().valid("incoming", "outgoing").required()
+});
+
 app.post ("/login", async (req, res) => {
     const {email, password} = req.body;
 
@@ -49,6 +56,7 @@ app.post ("/login", async (req, res) => {
         const user = await db.collection("users").findOne({email});
         if (!user) return res.sendStatus(404);
         if (!bcrypt.compareSync(password, user.password)) return res.sendStatus(401);
+        await db.collection("sessions").insertOne({userId: user._id, token});
         return res.status(200).send({token});
     } catch (err) {
         return res.status(500).send(err.message);
@@ -76,6 +84,42 @@ app.post ("/sign-up", async (req, res) => {
         return res.status(500).send(err.message);
     }
 });
+
+app.post ("/transaction/:type", async (req, res) => {
+    const {authorization} = req.headers;
+    let {value, description} = req.body;
+    const {type} = req.params;
+    const token = authorization?.replace("Bearer ", "");
+    
+    if (!token) return res.sendStatus(401);
+
+    const validation = transactionSchema.validate({token, type, value, description}, {abortEarly: false});
+    if(validation.error){
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    value = parseFloat(value);
+    value = value.toFixed(2);
+    
+    try{
+        
+        const session = await db.collection("sessions").findOne({token});
+        
+        if (!session) return res.sendStatus(401);
+        
+        const user = await db.collection("users").findOne({_id: session.userId});
+        if (!user) return res.send(401);
+        await db.collection("transactions").insertOne({value, description, type, userId: session.userId});
+        return res.sendStatus(200);
+    } catch (err){
+        return res.status(500).send(err.message);
+    }
+
+});
+
+
+
 
 const PORT = 5000;
 app.listen (PORT, () => console.log("Servidor rodando na porta 5000"));
